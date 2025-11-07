46909b2d85b09725cd400a848a57940608c0575a
h-alice <admin@halice.art>
2025-11-07 11:15:56 +0800

    Daemon pattern, new ENV field

--- src/main.rs
+++ src/main.rs
@@ -5,51 +5,173 @@ use diesel::{prelude::*};
 use diesel::r2d2::{ConnectionManager, Pool};
 
 use dotenv::dotenv;
+use tokio::select;
 use std::env;
 
+/// The full action: First getting live video ID from channel `handle`,
+/// then the live title with previously fetched video id, then store 
+/// the live information to local SQLite DB.
+/// 
+/// If bad thing happened, the error will be returned as `String`, convert
+/// to `Error` type if needed.
+async fn get_live_id_and_insert(pool : Pool<ConnectionManager<SqliteConnection>>, handle : String) 
+  -> Result<Option<LiveEntry>, String> {
 
-//use std::fmt;
+  // Get the live id
+  let live_id: String = 
+    live_id_from_channel_name(handle.as_str())
+      .await
+      .map_err(|err| err.to_string())?; // Stringify error
 
+  let title: String = 
+    video_title_from_id(live_id.as_str())
+      .await
+      .map_err(|err| err.to_string())?; // Stringify error
 
-async fn get_live_id_and_insert(pool : &Pool<ConnectionManager<SqliteConnection>>, handle : &str) 
-  -> Result<Option<LiveEntry>, Box<dyn std::error::Error>> {
+  // DB action in separated thread
+  let res: Option<LiveEntry> = tokio::task::spawn_blocking(move || {
 
-  let pool = pool.clone();
-  let live_id = live_id_from_channel_name(handle).await?;
-  let title = video_title_from_id(live_id.as_str()).await?;
+    // 
+    let mut conn = 
+      pool.get().map_err(|err| format!("cannot get connection: {err}"))?;
 
-  let res = tokio::task::spawn_blocking(move || {
-    let mut conn = pool.get().map_err(|err| format!("cannot get connection: {err}"))?;
-    let result = insert_live_info(&mut conn, &title, &live_id);
+    let result = 
+      insert_live_info(&mut conn, &title, &live_id);
+
     match result {
       Ok(inserted) => { Ok(inserted) }
       Err(e) => { Err(e.to_string()) }
     }
-  }).await?.map_err(|err_str| err_str)?;
+    
+  }).await
+    .map_err(|err| err.to_string())
+    .map_err(|err_str| err_str)??;
   
   return Ok(res);
+}
+
+/// The daemon part, it repeatly execute info fetcher, and terminate if
+/// received cancel signal.
+/// 
+/// If bad thing happened, the error will be returned as `String`, convert
+/// to `Error` type if needed.
+async fn daemon(cancel_tx : tokio::sync::broadcast::Sender<()>) 
+  -> Result<(), String> {
+
+  use tokio::time::interval;
+
+  // Initial setup
+
+  dotenv().ok(); // Check .env
+
+  let pool = mk_connection_pool()
+    .map_err(|err| err.to_string())?;
+
+  // init: get channels to track
+  let tracking_channels_env = 
+    env::var("CHANNELS")
+      .map_err(|err| format!("env error: {err}"))?;
+
+  let interval_time_sec_env = 
+    env::var("REPEAT_INTERVAL")
+      .map(|s| s.parse::<u64>())
+      .unwrap()
+      .unwrap_or(100);
+  
+  let tracking_channels = 
+    tracking_channels_env.split(";")
+      .filter(|x| !x.is_empty())
+      .map(|s| s.to_string())
+      .collect::<Vec<String>>();
+
+  // init: setup time tick
+  // TODO: remove hardcoded time
+  let mut tick = interval(tokio::time::Duration::from_secs(interval_time_sec_env));
+
+  // Check if there's any channel name provided.
+  if tracking_channels.is_empty() {
+    return Err("no channels to track".into());
+  }
+
+  // signal watcher
+  let mut cancel_rx = cancel_tx.subscribe();
+  
+  loop { // Actual daemon part
+
+    select! {
+      
+      _ = tick.tick() => {
+
+        // dispatch handle names into fetcher tasks
+        let tasks  = 
+          tracking_channels.iter().map(|c| {
+            get_live_id_and_insert(pool.clone(), c.to_string())
+          });
+        
+        // create JoinSet from previous result
+        let task_set = tokio::task::JoinSet::from_iter(tasks);
+
+        // await task results
+        let result = task_set.join_all().await;
+
+        println!("result: {:?}", result); // debug print
+        
+      }
+
+      _ = cancel_rx.recv() => {
+        println!("[Daemon] Shutdown signal received. Cleaning up...");
+        // TODO: maybe some cleanup here.
+        println!("[Daemon] Task terminated gracefully.");
+
+        return Ok(()); // bye bye
+      }
+    }
+  }
+}
+
+async fn cancel_sig_watcher(tx : tokio::sync::broadcast::Sender<()>)
+  -> Result<(), String> {
+
+  use tokio::signal;
+  use tokio::signal::unix::{signal, SignalKind};
+
+
+  let mut signal_stream = 
+    signal(SignalKind::terminate()).map_err(|e| e.to_string())?;
+
+  tokio::select! {
+
+    _ = signal::ctrl_c() => {
+      println!("[Watcher] Received Ctrl+C signal.");
+    }
+
+    _ = signal_stream.recv() => {
+      println!("[Watcher] Received SIGTERM signal.");
+    }
+  }
+
+  // Send the shutdown signal.
+  tx.send(()).map(|_| ()).map_err(|e| e.to_string())
+
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-  dotenv().ok();
-  let tracking_channels_env = env::var("CHANNELS").map_err(|err| format!("env error: {err}"))?;
+  use tokio::sync::broadcast;
 
-  let tracking_channels = tracking_channels_env.split(";").filter(|x| !x.is_empty()).collect::<Vec<&str>>();
+  let (cancel_tx , mut _cancel_rx) = 
+    broadcast::channel::<()>(5);
 
-  if tracking_channels.is_empty() {
-    return Err("no channels to track".into());
-  }
+  // install the signal handler.
+  let hdle_watcher = 
+    tokio::spawn(cancel_sig_watcher(cancel_tx.clone()));
 
-  let pool = mk_connection_pool()?;
+  // start the daemon.
+  let hdle_daemon = 
+    tokio::spawn(daemon(cancel_tx.clone()));
 
-  let res = get_live_id_and_insert(&pool, tracking_channels[0]).await?;
+  let _ = tokio::join!(hdle_watcher, hdle_daemon);
 
-  match res {
-    Some(row) => println!("successfull insert: \n{:?}", row),
-    None => println!("Live database remain unchanged, record may have already existed"),
-  }
-
-  return Ok(());
+  return Ok(()); // bye bye
 }
